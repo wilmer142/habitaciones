c347382052bece725983d1a579dbec9298488aa3
wilmer142 <wilmercollege@gmail.com>
2019-01-27 22:16:59 -0500

    Solve error on model validate

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,5 @@ class Room < ApplicationRecord
 
 	validates :title, :description, :beds, :guests, :image_url, presence: true
 	validates :description, length: {maximum: 400}
-	validades :beds, :guests, numericality: {only_integer: true}
+	validates :beds, :guests, numericality: {only_integer: true}
 end
